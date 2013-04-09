89a0efec75c5fe0e879d5988234023b47d394450
Graham Tremper <gtremper@gmail.com>
2013-04-08 23:09:27 -0700

    did it better

--- main.cpp
+++ main.cpp
@@ -117,7 +117,7 @@ void init() {
     
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-1,1,1,-1,-1,1);
+	glOrtho(-1,1,-1,1,-1,1);
 	glMatrixMode(GL_MODELVIEW);
 	glm::mat4 mv = glm::lookAt(glm::vec3(0,0,1),glm::vec3(0,0,0),glm::vec3(0,1,0));
 	glLoadMatrixf(&mv[0][0]);
@@ -133,10 +133,10 @@ void display(){
 
 
 	glBegin(GL_QUADS);
-	glTexCoord2d(0, 0); glVertex3d(-1, -1, 0);
-	glTexCoord2d(0, 1); glVertex3d(-1, 1, 0);
-	glTexCoord2d(1, 1); glVertex3d(1, 1, 0);
-	glTexCoord2d(1, 0); glVertex3d(1, -1, 0);
+	glTexCoord2d(0, 1); glVertex3d(-1, -1, 0);
+	glTexCoord2d(0, 0); glVertex3d(-1, 1, 0);
+	glTexCoord2d(1, 0); glVertex3d(1, 1, 0);
+	glTexCoord2d(1, 1); glVertex3d(1, -1, 0);
 	glEnd();
 
 	glutSwapBuffers();
