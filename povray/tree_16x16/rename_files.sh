9e3c8eb5864738c3eec08345eab9a0c7d4e4cd9a
Gabe Fierro <gtfierro225@gmail.com>
2013-04-16 15:40:40 -0700

    remove old 16x16 files

--- povray/tree_16x16/rename_files.sh
+++
@@ -1,6 +0,0 @@
-index=0;
-for name in *.png
-do
-  mv "${name}" `printf "%04d" $index`.png
-  index=$((index+1))
-done
--- povray/tree_16x16/render_tree.sh
+++
@@ -1,6 +0,0 @@
-povray tree0.ini
-povray tree1.ini
-povray tree2.ini
-povray tree3.ini
-povray tree4.ini
-povray tree5.ini
